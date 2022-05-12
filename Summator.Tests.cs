77c234531ad258d18fd7a4d56250bd7020d672e8
Statevbg <75937149+Statevbg@users.noreply.github.com>
2022-05-12 16:02:29 +0300

    Unit.Tests

---
+++ Summator.Tests.cs
@@ -0,0 +1,90 @@
+using System;
+using NUnit.Framework;
+
+namespace Summator.Tests
+{
+	public class Tests
+	{
+		[Test]
+		public void Test_Sum_TwoPossitiveNumbers()
+		{
+			//Arrange
+			int[] nums = new int[]{3, 9 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == 12);
+
+
+		}
+
+		[Test]
+		public void Test_Sum_OnePossitiveNumbers()
+		{
+			//Arrange
+			int[] nums = new int[] { 3 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == 3);
+		}
+		[Test]
+		public void Test_Sum_TwoNegative()
+		{
+			//Arrange
+			int[] nums = new int[] { -3, -9 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == -12);
+		}
+
+		[Test]
+		public void Test_Sum_OnePossitiveOneNegative()
+		{//Arrange
+			int[] nums = new int[] { 3, -9 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == -6);
+		}
+		[Test]
+		public void Test_Sum_OneNegativeOnePossitive()
+		{
+			//Arrange
+			int[] nums = new int[] { -3, 7 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == 4);
+		}
+		[Test]
+		public void Test_Sum_OnePossitvieOneNegativeAndZero()
+		{
+			//Arrange
+			int[] nums = new int[] { 3, -6, 0 };
+
+			//Act
+			int result = Summator.Sum(nums);
+
+			//Assert
+			Assert.That(result == -3);   
+		}
+
+	
+
+
+
+
+	}
+}
